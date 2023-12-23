d6589618a48747e3469f812300577a8aac3f6bad
MintChocoManju <mnln2468@gmail.com>
2023-12-23 16:53:13 +0800

    Cache testing branch

--- 01_RTL/CHIP.v
+++ 01_RTL/CHIP.v
@@ -380,31 +380,31 @@ module SLT_unit #(
 endmodule
 
 module Cache#(
-        parameter BIT_W = 32,
-        parameter ADDR_W = 32
-    )(
-        input i_clk,
-        input i_rst_n,
-        // processor interface
-            input i_proc_cen,
-            input i_proc_wen,
-            input [ADDR_W-1:0] i_proc_addr,
-            input [BIT_W-1:0]  i_proc_wdata,
-            output [BIT_W-1:0] o_proc_rdata,
-            output o_proc_stall,
-            input i_proc_finish,
-            output o_cache_finish,
-        // memory interface
-            output o_mem_cen,
-            output o_mem_wen,
-            output [ADDR_W-1:0] o_mem_addr,
-            output [BIT_W*4-1:0]  o_mem_wdata,
-            input [BIT_W*4-1:0] i_mem_rdata,
-            input i_mem_stall,
-            output o_cache_available,
-        // others
-        input  [ADDR_W-1: 0] i_offset
-    );
+    parameter BIT_W = 32,
+    parameter ADDR_W = 32
+) (
+    input i_clk,
+    input i_rst_n,
+    // processor interface
+    input i_proc_cen,
+    input i_proc_wen,
+    input [ADDR_W-1:0] i_proc_addr,
+    input [BIT_W-1:0]  i_proc_wdata,
+    output [BIT_W-1:0] o_proc_rdata,
+    output o_proc_stall,
+    input i_proc_finish,
+    output o_cache_finish,
+    // memory interface
+    output o_mem_cen,
+    output o_mem_wen,
+    output [ADDR_W-1:0] o_mem_addr,
+    output [BIT_W*4-1:0]  o_mem_wdata,
+    input [BIT_W*4-1:0] i_mem_rdata,
+    input i_mem_stall,
+    output o_cache_available,
+    // others
+    input  [ADDR_W-1: 0] i_offset
+);
 
     assign o_cache_available = 0; // change this value to 1 if the cache is implemented
 
